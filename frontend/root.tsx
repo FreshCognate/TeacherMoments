4d94ed3a3bf62faf34a30c511f6903973131dd0f
darylhedley <darylhedley@hotmail.com>
2025-01-13 09:52:34 +0000

    Fixes padding top

--- frontend/root.tsx
+++ frontend/root.tsx
@@ -72,11 +72,11 @@ export default function App(props) {
   }
 
   return (
-    <>
+    <div className="pt-10">
       <DialogsContainer />
       <NavigationContainer authentication={props.loaderData.authentication} />
       <Outlet />
-    </>
+    </div>
   );
 }
 
@@ -97,7 +97,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="pt-10 p-4 container mx-auto">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
