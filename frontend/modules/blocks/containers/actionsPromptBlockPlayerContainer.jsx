7890ceeebb20482efec5550a48f46e37e717ddcb
darylhedley <darylhedley@hotmail.com>
2025-01-31 16:12:46 +0000

    Adds reset scenario action ability

--- frontend/modules/blocks/containers/actionsPromptBlockPlayerContainer.jsx
+++ frontend/modules/blocks/containers/actionsPromptBlockPlayerContainer.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import ActionsPromptBlockPlayer from '../components/actionsPromptBlockPlayer';
 import setSlideToComplete from '~/modules/tracking/helpers/setSlideToComplete';
+import updateTracking from '~/modules/tracking/helpers/updateTracking';
+import resetScenario from '~/modules/tracking/helpers/resetScenario';
 
 class ActionsPromptBlockPlayerContainer extends Component {
 
   onActionClicked = (action) => {
-    console.log(action);
+    this.props.onUpdateTracking({ isComplete: true });
     switch (action.actionType) {
       case 'COMPLETE_SLIDE':
-        console.log('Should complete slide');
         setSlideToComplete({ slideRef: this.props.block.slideRef });
         break;
-      case 'RESTART_SCENARIO':
-        console.log('Should restart scenario');
+      case 'RESET_SCENARIO':
+        resetScenario();
         break;
     }
   }
