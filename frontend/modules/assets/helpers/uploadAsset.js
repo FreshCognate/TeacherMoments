4c7e7cb4a0c36c493d7b02e274d5c115a96e0d14
darylhedley <darylhedley@hotmail.com>
2025-02-14 08:20:16 +0000

    Start of adding creation of assets

--- frontend/modules/assets/helpers/uploadAsset.js
+++ frontend/modules/assets/helpers/uploadAsset.js
@@ -1,3 +1,13 @@
-export default ({ file }) => {
+import axios from 'axios';
+
+export default async ({ file }) => {
   console.log('uploadAsset', file);
+  const asset = await axios.post('/api/assets', { name: file.name, fileType: file.type });
+  console.log(asset);
+  // await axios.post('/api/assets/signedUrl', {
+  //   size: file.size,
+  //   type: file.type
+  // });
+
+  return "EVENT";
 }
