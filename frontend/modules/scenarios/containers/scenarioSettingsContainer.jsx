35dda0eda43311ee7f7e56e18e30420c88037ce7
darylhedley <darylhedley@hotmail.com>
2025-02-25 17:01:08 +0000

    Enables deleting of scenario

--- frontend/modules/scenarios/containers/scenarioSettingsContainer.jsx
+++ frontend/modules/scenarios/containers/scenarioSettingsContainer.jsx
@@ -3,11 +3,37 @@ import ScenarioSettings from '../components/scenarioSettings';
 import WithCache from '~/core/cache/containers/withCache';
 import WithRouter from '~/core/app/components/withRouter';
 import editScenarioSchema from '../schemas/editScenarioSchema';
+import addModal from '~/core/dialogs/helpers/addModal';
+import handleRequestError from '~/core/app/helpers/handleRequestError';
+import axios from 'axios';
 
 class ScenarioSettingsContainer extends Component {
 
   onUpdateScenario = ({ update }) => {
     return this.props.scenario.mutate(update, { method: 'put' });
+  }
+
+  onDeleteScenarioClicked = () => {
+    addModal({
+      title: 'Delete scenario',
+      body: `Are you sure you would like to delete "${this.props.scenario.data.name}"?`,
+      actions: [{
+        type: 'CANCEL',
+        text: 'Cancel'
+      }, {
+        type: 'DELETE',
+        text: 'Delete',
+        color: 'warning'
+      }]
+    }, (state, { type, modal }) => {
+      if (state === 'ACTION') {
+        if (type === 'DELETE') {
+          axios.delete(`/api/scenarios/${this.props.scenario.data._id}`).then((response) => {
+            this.props.router.navigate(`/scenarios`);
+          }).catch(handleRequestError);
+        }
+      }
+    })
   }
 
   render() {
@@ -18,6 +44,7 @@ class ScenarioSettingsContainer extends Component {
         scenario={data}
         isLoading={status === 'loading' || status === 'unresolved'}
         onUpdateScenario={this.onUpdateScenario}
+        onDeleteScenarioClicked={this.onDeleteScenarioClicked}
       />
     );
   }
