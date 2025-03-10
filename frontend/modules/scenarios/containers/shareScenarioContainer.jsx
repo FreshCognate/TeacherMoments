dc61aa3af6ab127ade7e6724bb55f9da42512743
darylhedley <darylhedley@hotmail.com>
2025-03-10 12:55:44 +0000

    Adds basic share UI to publish

--- frontend/modules/scenarios/containers/shareScenarioContainer.jsx
+++ frontend/modules/scenarios/containers/shareScenarioContainer.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import ShareScenario from '../components/shareScenario';
+import WithCache from '~/core/cache/containers/withCache';
+import handleRequestError from '~/core/app/helpers/handleRequestError';
+import axios from 'axios';
+import WithRouter from '~/core/app/components/withRouter';
 
 class ShareScenarioContainer extends Component {
+
+  state = {
+    isPublishing: false,
+  }
+
+  onPublishScenarioClicked = () => {
+    this.setState({ isPublishing: true });
+    axios.post(`/api/publishes`, {
+      scenarioId: this.props.scenario.data._id
+    }).then(() => {
+      this.props.scenario.fetch();
+      this.setState({ isPublishing: false });
+    }).catch((error) => {
+      handleRequestError(error);
+      this.setState({ isPublishing: false });
+    })
+  }
+
   render() {
     return (
-      <ShareScenario />
+      <ShareScenario
+        scenario={this.props.scenario.data}
+        isPublishing={this.state.isPublishing}
+        onPublishScenarioClicked={this.onPublishScenarioClicked}
+      />
     );
   }
 };
 
-export default ShareScenarioContainer;
+export default WithRouter(WithCache(ShareScenarioContainer, {}, ['scenario']));
