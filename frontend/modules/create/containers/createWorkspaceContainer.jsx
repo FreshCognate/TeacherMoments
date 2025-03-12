3e31d0fc5b0bb89c301ef0794e9986755213de8c
darylhedley <darylhedley@hotmail.com>
2025-03-12 18:04:03 +0000

    Adds basic workspace panel

---
+++ frontend/modules/create/containers/createWorkspaceContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CreateWorkspace from '../components/createWorkspace';
+
+class CreateWorkspaceContainer extends Component {
+  render() {
+    return (
+      <CreateWorkspace />
+    );
+  }
+};
+
+export default CreateWorkspaceContainer;
