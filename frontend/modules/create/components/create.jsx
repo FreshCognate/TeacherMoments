7f6e04e9234649dc7885ae39d7afc1c75d0d3b4d
darylhedley <darylhedley@hotmail.com>
2025-03-12 17:51:03 +0000

    Creates a navigation container

--- frontend/modules/create/components/create.jsx
+++ frontend/modules/create/components/create.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CreateNavigationContainer from '../containers/createNavigationContainer';
 
 const Create = ({
 }) => {
@@ -6,7 +7,9 @@ const Create = ({
     <div
       id="scenario-builder"
       style={{ height: 'calc(100vh - 68px', marginTop: '28px' }}
+      className="bg-lm-2 dark:bg-dm-2"
     >
+      <CreateNavigationContainer />
     </div>
   );
 };
