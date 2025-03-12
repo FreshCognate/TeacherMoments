764a90f655471c52dd3fc5a118ac510be4689843
darylhedley <darylhedley@hotmail.com>
2025-03-12 17:41:01 +0000

    Adds basic create contianer

---
+++ frontend/modules/create/containers/createContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Create from '../components/create';
+
+class CreateContainer extends Component {
+  render() {
+    return (
+      <Create />
+    );
+  }
+};
+
+export default CreateContainer;
--- frontend/modules/scenarios/routes/createScenarioRoute.tsx
+++ frontend/modules/scenarios/routes/createScenarioRoute.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import CreateContainer from '~/modules/create/containers/createContainer';
 import ScenarioBuilderContainer from '~/modules/scenarioBuilder/containers/scenarioBuilderContainer';
 
 export default function CreateScenarioRoute() {
+  return <CreateContainer />
   return <ScenarioBuilderContainer />
 }
