9102826bacfe29a5e5b34c77424c91eb73457047
darylhedley <darylhedley@hotmail.com>
2025-03-17 14:12:17 +0000

    Adds new slide between slides

--- frontend/modules/create/containers/createNavigationContainer.jsx
+++ frontend/modules/create/containers/createNavigationContainer.jsx
@@ -6,6 +6,7 @@ import filter from 'lodash/filter';
 import axios from 'axios';
 import handleRequestError from '~/core/app/helpers/handleRequestError';
 import getUrlDetails from '../helpers/getUrlDetails';
+import find from 'lodash/find';
 
 class CreateNavigationContainer extends Component {
 
@@ -21,16 +22,25 @@ class CreateNavigationContainer extends Component {
     })
   }
 
+  getSelectedSlideSortOrder = () => {
+    const { selectedSlideId } = getUrlDetails();
+    return find(this.props.slides.data, { _id: selectedSlideId }).sortOrder;
+  }
+
   onAddSlideClicked = () => {
     let parentRef;
     this.setState({ isCreating: true });
+    const scenarioId = this.props.scenario.data._id;
     axios.post(`/api/slides`, {
       scenarioId: this.props.scenario.data._id,
       parentRef,
-      sortOrder: this.getCurrentStemOfSlides().length
-    }).then(() => {
-      this.props.slides.fetch();
-      this.setState({ isCreating: false });
+      sortOrder: this.getSelectedSlideSortOrder() + 1
+    }).then((response) => {
+      const slideId = response.data.slide._id;
+      this.props.slides.fetch().then(() => {
+        this.props.router.navigate(`/scenarios/${scenarioId}/create?slide=${slideId}`)
+        this.setState({ isCreating: false });
+      });
     }).catch((error) => {
       this.props.slides.fetch();
       this.setState({ isCreating: false });
