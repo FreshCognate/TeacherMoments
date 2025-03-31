2254e0e3a6ef5aad6784f4456e1af29d12d56d6b
darylhedley <darylhedley@hotmail.com>
2025-03-31 21:28:24 +0100

    Enables back button again

--- frontend/modules/slides/components/slidePlayerHeader.jsx
+++ frontend/modules/slides/components/slidePlayerHeader.jsx
@@ -8,9 +8,6 @@ const SlidePlayerHeader = ({
   return (
     <div className="flex items-center justify-between p-4">
       <div>
-        {(hasBackButton) && (
-          <FlatButton icon="back" onClick={onPreviousSlideClicked} />
-        )}
       </div>
       <div>
         <FlatButton icon="menu" />
