4c3443c6dfe16279353005d1596d05d3af354e6c
darylhedley <darylhedley@hotmail.com>
2025-04-01 10:53:23 +0100

    Fixes styling of toggle

--- frontend/uikit/toggles/components/toggle.jsx
+++ frontend/uikit/toggles/components/toggle.jsx
@@ -5,28 +5,27 @@ import classnames from 'classnames';
 const Toggle = ({
   value,
   options,
-  color = "primary",
   className,
   size,
   isDisabled,
   onClick
 }) => {
-  const classNames = classnames('inline-flex items-center rounded overflow-hidden', className);
+  const classNames = classnames('inline-flex items-center rounded-md overflow-hidden p-0.5 bg-lm-2 dark:bg-dm-2', className);
   return (
     <div className={classNames}>
       {map(options, (option) => {
         const isSelected = value === option.value;
 
-        let className = classnames("bg-lm-0 dark:bg-dm-0", {
+        let className = classnames("rounded-md", {
           'p-2': size !== 'sm',
           'py-1 px-2 text-xs': size === 'sm',
-          'opacity-40': !isSelected,
-          'opacity-100 dark:bg-white dark:bg-opacity-10': isSelected,
+          'opacity-100': !isSelected,
+          'opacity-100 shadow-sm bg-lm-0 dark:bg-dm-0': isSelected,
         });
 
-        let selectedColor = isSelected ? color : null;
+
         return (
-          <FlatButton color={selectedColor} size={size} isDisabled={isDisabled} className={className} key={option.value} {...option} onClick={(event) => {
+          <FlatButton size={size} isDisabled={isDisabled} className={className} key={option.value} {...option} onClick={(event) => {
             event.stopPropagation();
             onClick(option.value);
           }} />
