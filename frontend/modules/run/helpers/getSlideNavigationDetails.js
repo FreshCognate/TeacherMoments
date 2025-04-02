180abfb5e7c1d50dee1193587b7a4edc58241776
darylhedley <darylhedley@hotmail.com>
2025-04-02 13:16:03 +0100

    Fixes navigation of submit button

--- frontend/modules/run/helpers/getSlideNavigationDetails.js
+++ frontend/modules/run/helpers/getSlideNavigationDetails.js
@@ -1,17 +1,21 @@
 import getBlocksBySlideRef from "~/modules/blocks/helpers/getBlocksBySlideRef";
 import getSlideStage from "./getSlideStage"
 import getBlockTracking from "./getBlockTracking";
+import getBlockDisplayType from "~/modules/blocks/helpers/getBlockDisplayType";
 
 export default () => {
   let isAbleToCompleteSlide = true;
   let hasRequiredPrompts = false;
+  let hasPrompts = false;
   const slideStage = getSlideStage();
   if (slideStage) {
 
     const blocks = getBlocksBySlideRef({ slideRef: slideStage.slideRef });
 
     for (const block of blocks) {
-
+      if (getBlockDisplayType(block) === 'PROMPT') {
+        hasPrompts = true;
+      }
       if (block.isRequired) {
         hasRequiredPrompts = true;
         const blockTracking = getBlockTracking({ blockRef: block.ref });
@@ -24,5 +28,5 @@ export default () => {
     }
 
   }
-  return { isAbleToCompleteSlide, hasRequiredPrompts };
+  return { isAbleToCompleteSlide, hasRequiredPrompts, hasPrompts };
 }
