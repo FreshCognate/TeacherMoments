c83378971543476023b7cabbb5e3d01827697962
darylhedley <darylhedley@hotmail.com>
2025-04-15 14:06:25 +0100

    Fixes issue with routing and unpublished scenarios

--- backend/modules/scenarios/services/getScenarioByPublishLink.js
+++ backend/modules/scenarios/services/getScenarioByPublishLink.js
@@ -5,9 +5,9 @@ export default async (props, options, context) => {
 
   const scenario = await models.Published_Scenario.findOne({ publishLink: publishLink, isDeleted: false });
 
-  if (!scenario) throw { message: 'This scenario does not exist', statusCode: 404 };
+  if (!scenario) throw { message: 'This scenario does not exist', statusCode: 404, shouldRedirectToScenarios: true };
 
-  if (!scenario.isPublished) throw { message: 'This scenario is not published', statusCode: 404 };
+  if (!scenario.isPublished) throw { message: 'This scenario is not published', statusCode: 404, shouldRedirectToScenarios: true };
 
   return scenario;
 
