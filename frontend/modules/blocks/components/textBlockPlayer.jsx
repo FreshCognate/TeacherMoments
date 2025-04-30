704e1efd0a874e4671192054a0efb52108ca801d
darylhedley <darylhedley@hotmail.com>
2025-04-30 18:03:34 +0100

    Sets body to the same as question text

--- frontend/modules/blocks/components/textBlockPlayer.jsx
+++ frontend/modules/blocks/components/textBlockPlayer.jsx
@@ -9,7 +9,7 @@ const TextBlockPlayer = ({
   return (
     <div>
       <Title title={getString({ model: block, field: 'title' })} className="text-xl mb-2" />
-      <Body body={getString({ model: block, field: 'body' })} className="text-sm text-black text-opacity-60 dark:text-white dark:text-opacity-60" />
+      <Body body={getString({ model: block, field: 'body' })} className="text-black dark:text-white" />
     </div>
   );
 };
