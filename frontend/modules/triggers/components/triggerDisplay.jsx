18b5d81f38a9b2499e56f04a1641b6b9dd26c984
darylhedley <darylhedley@hotmail.com>
2025-06-05 10:50:30 +0100

    Adds basic slide trigger

--- frontend/modules/triggers/components/triggerDisplay.jsx
+++ frontend/modules/triggers/components/triggerDisplay.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import FlatButton from '~/uikit/buttons/components/flatButton';
+import Alert from '~/uikit/alerts/components/alert';
+import map from 'lodash/map';
+import Body from '~/uikit/content/components/body';
 import Icon from '~/uikit/icons/components/icon';
+import Button from '~/uikit/buttons/components/button';
+import TriggerItem from './triggerItem';
 
 const TriggerDisplay = ({
-  eventDescription,
-  triggersCount,
-  onOpenTriggerPanelClicked
+  triggers,
+  onAddTriggerClicked
 }) => {
   return (
-    <div className="bg-lm-3/50 dark:bg-dm-3/50 text-xs rounded flex items-center overflow-auto">
-      <div className="bg-lm-3/50 dark:bg-dm-3/50 p-4 mr-2 border-r border-lm-3 dark:border-dm-3">
-        <Icon icon="trigger" size="12" />
+    <div className="w-full pt-4 pb-8 px-8 max-w-screen-lg mx-auto">
+      <div className="flex items-center justify-between text-black/60 dark:text-white/60 mb-2">
+        <div className="flex items-center">
+
+          <Icon icon="trigger" size={16} className="mr-2" />
+          <Body body="Triggers" />
+        </div>
+        <Alert text="Triggers are extra functionality that can happen during the transition to the next slide." type="info" />
       </div>
-      <div>
-        <FlatButton
-          text={`${eventDescription}: ${triggersCount} trigger${triggersCount === 1 ? '' : 's'}`}
-          size="sm"
-          color="primary"
-          onClick={onOpenTriggerPanelClicked}
-        />
+      <div className="bg-lm-0 dark:bg-dm-0 p-2 rounded-md">
+        <div>
+          {map(triggers, (trigger) => {
+            return (
+              <div key={trigger._id} className="mb-2">
+                <TriggerItem
+                  trigger={trigger}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <Button color="primary" text="Add trigger" icon="trigger" onClick={onAddTriggerClicked} />
+        </div>
       </div>
     </div>
   );
