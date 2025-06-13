b8da0975de51d64055e932e181ae1352916de450
darylhedley <darylhedley@hotmail.com>
2025-06-13 12:01:00 +0100

    Fixes bug with undefined ref

--- frontend/modules/triggers/containers/triggerDisplayContainer.jsx
+++ frontend/modules/triggers/containers/triggerDisplayContainer.jsx
@@ -15,7 +15,7 @@ class TriggerDisplayContainer extends Component {
 
   getTriggers = () => {
     const { data } = this.props.triggers;
-    return filter(data, (trigger) => trigger.elementRef === this.props.slide.data.ref && trigger.event === 'ON_EXIT');
+    return filter(data, (trigger) => trigger.elementRef === this.props.slide.data?.ref && trigger.event === 'ON_EXIT');
   }
 
   getTriggerBaseModel = () => {
