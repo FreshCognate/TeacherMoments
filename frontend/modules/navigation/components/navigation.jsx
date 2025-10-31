d98ef2287915abaa48afeedac99b0ce4b009d8fe
darylhedley <darylhedley@hotmail.com>
2025-10-31 10:20:52 +0000

    Adds active

--- frontend/modules/navigation/components/navigation.jsx
+++ frontend/modules/navigation/components/navigation.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 import { Link, NavLink } from 'react-router';
 
@@ -8,13 +9,34 @@ const Navigation = ({
     <div className="flex items-center fixed w-full top-0 z-30 justify-between h-14 px-4  bg-lm-0 dark:bg-dm-0 border-b border-b-lm-3 dark:border-b-dm-2">
       <div>
         <nav className="flex gap-x-4">
-          <NavLink to="/" className="text-black/60 dark:text-white/60 hover:text-black/80 dark:hover:text-white/80 transition-colors">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              classNames("text-black/60 dark:text-white/60 hover:text-black/80 dark:hover:text-white/80 transition-colors", {
+                "underline": isActive
+              })
+            }
+          >
             Dashboard
           </NavLink>
-          <NavLink to="/scenarios" className="text-black/60 dark:text-white/60 hover:text-black/80 dark:hover:text-white/80 transition-colors">
+          <NavLink
+            to="/scenarios"
+            className={({ isActive }) =>
+              classNames("text-black/60 dark:text-white/60 hover:text-black/80 dark:hover:text-white/80 transition-colors", {
+                "underline": isActive
+              })
+            }
+          >
             Scenarios
           </NavLink>
-          <NavLink to="/cohorts" className="text-black/60 dark:text-white/60 hover:text-black/80 dark:hover:text-white/80 transition-colors">
+          <NavLink
+            to="/cohorts"
+            className={({ isActive }) =>
+              classNames("text-black/60 dark:text-white/60 hover:text-black/80 dark:hover:text-white/80 transition-colors", {
+                "underline": isActive
+              })
+            }
+          >
             Cohorts
           </NavLink>
         </nav>
