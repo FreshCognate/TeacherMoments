2a8ce07b15cbee9c26e9c517a8b2fb07d1ef4fca
darylhedley <darylhedley@hotmail.com>
2025-12-01 19:22:04 +0000

    Adds a screen for empty list

--- frontend/modules/cohorts/components/cohortScenariosList.tsx
+++ frontend/modules/cohorts/components/cohortScenariosList.tsx
@@ -14,6 +14,8 @@ import {
 import { Scenario } from '~/modules/scenarios/scenarios.types';
 import CohortScenarioListItem from './cohortScenariosListItem';
 import map from 'lodash/map';
+import Title from '~/uikit/content/components/title';
+import Body from '~/uikit/content/components/body';
 
 
 const CohortScenariosList = ({
@@ -32,6 +34,14 @@ const CohortScenariosList = ({
 
   return (
     <div className="flex gap-y-2 flex-col">
+      {(scenarios?.length === 0) && (
+        <div className="text-center py-8 px-6 border border-lm-1 dark:border-dm-1 rounded-xl bg-lm-1 dark:bg-dm-1">
+          <div className="max-w-md mx-auto">
+            <Title title="No Scenarios have been added to this Cohort" size='xl' className="mb-4" />
+            <Body body="Search and find Scenarios in the list opposite and click 'Add' to add a Scenario to this Cohort." size='sm' className="text-white/50" />
+          </div>
+        </div>
+      )}
       <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={onDragEnd}>
         <SortableContext items={map(scenarios, '_id')} strategy={verticalListSortingStrategy}>
           {map(scenarios, (scenario: Scenario) => (
