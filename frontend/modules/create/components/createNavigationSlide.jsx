f474dc57750865d5a4b07b9649760f617c33e3b4
darylhedley <darylhedley@hotmail.com>
2026-01-24 20:48:50 +0000

    Reverts clicking

--- frontend/modules/create/components/createNavigationSlide.jsx
+++ frontend/modules/create/components/createNavigationSlide.jsx
@@ -4,7 +4,6 @@ import classnames from 'classnames';
 import CreateNavigationSlideActionsContainer from '../containers/createNavigationSlideActionsContainer';
 import map from 'lodash/map';
 import getBlockComponent from '~/modules/blocks/helpers/getBlockComponent';
-import navigateTo from '~/modules/run/helpers/navigateTo';
 
 const CreateNavigationSlide = ({
   scenarioId,
@@ -27,53 +26,50 @@ const CreateNavigationSlide = ({
   });
 
   return (
-    <div onClick={() => navigateTo({ slideRef })}>
+    <Link to={`/scenarios/${scenarioId}/create?slide=${slide._id}`} replace>
+      <div className={className} style={style} ref={setNodeRef} {...listeners} {...attributes}>
+        <CreateNavigationSlideActionsContainer
+          slideNumber={slide.sortOrder + 1}
+          canDeleteSlides={canDeleteSlides}
+          onDuplicateSlideClicked={() => onDuplicateSlideClicked(slide._id)}
+          onDeleteSlideClicked={() => onDeleteSlideClicked(slide._id)}
+        />
+        <div>
 
-      <Link to={`/scenarios/${scenarioId}/create?slide=${slide._id}`} replace>
-        <div className={className} style={style} ref={setNodeRef} {...listeners} {...attributes}>
-          <CreateNavigationSlideActionsContainer
-            slideNumber={slide.sortOrder + 1}
-            canDeleteSlides={canDeleteSlides}
-            onDuplicateSlideClicked={() => onDuplicateSlideClicked(slide._id)}
-            onDeleteSlideClicked={() => onDeleteSlideClicked(slide._id)}
-          />
-          <div>
+          <div className="overflow-hidden h-28 rounded-b-lg">
 
-            <div className="overflow-hidden h-28 rounded-b-lg">
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="640" height="1000">
-                <foreignObject transform={'scale(0.376)'} width={'100%'} height={'100%'}>
-                  <section>
-                    {map(slideBlocks, (block) => {
-                      let Block = getBlockComponent({ blockType: block.blockType });
-                      return (
-                        <div
-                          key={block._id}
-                          className="mb-8 last:mb-0 p-4"
-                        >
-                          <Block
-                            block={block}
-                            blockTracking={{}}
-                          />
-                        </div>
-                      );
-                    })}
-                  </section>
-                </foreignObject>
-                <rect
-                  x="0"
-                  y="0"
-                  fill="transparent"
-                  transform={'scale(1)'}
-                  width={'100%'}
-                  height={'100%'}
-                />
-              </svg>
-            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="640" height="1000">
+              <foreignObject transform={'scale(0.376)'} width={'100%'} height={'100%'}>
+                <section>
+                  {map(slideBlocks, (block) => {
+                    let Block = getBlockComponent({ blockType: block.blockType });
+                    return (
+                      <div
+                        key={block._id}
+                        className="mb-8 last:mb-0 p-4"
+                      >
+                        <Block
+                          block={block}
+                          blockTracking={{}}
+                        />
+                      </div>
+                    );
+                  })}
+                </section>
+              </foreignObject>
+              <rect
+                x="0"
+                y="0"
+                fill="transparent"
+                transform={'scale(1)'}
+                width={'100%'}
+                height={'100%'}
+              />
+            </svg>
           </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
