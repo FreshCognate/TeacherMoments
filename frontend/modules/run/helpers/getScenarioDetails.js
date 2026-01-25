7f494d03bccfcc4e192a76e0e49a245a6184783c
darylhedley <darylhedley@hotmail.com>
2026-01-25 15:24:19 +0000

    Fixes activeSlideId

--- frontend/modules/run/helpers/getScenarioDetails.js
+++ frontend/modules/run/helpers/getScenarioDetails.js
@@ -1,8 +1,22 @@
+import getCache from '~/core/cache/helpers/getCache';
+import find from 'lodash/find';
+
 export default () => {
   const searchParams = new URLSearchParams(window.location.search);
   const activeSlideRef = searchParams.get('slide');
 
+  let activeSlideId = null;
+
+  if (activeSlideRef === 'CONSENT' || activeSlideRef === 'SUMMARY') {
+    activeSlideId = activeSlideRef;
+  } else {
+    const slides = getCache('slides');
+    const activeSlide = find(slides?.data, { ref: activeSlideRef });
+    activeSlideId = activeSlide?._id || null;
+  }
+
   return {
-    activeSlideRef
+    activeSlideRef,
+    activeSlideId
   }
 }
