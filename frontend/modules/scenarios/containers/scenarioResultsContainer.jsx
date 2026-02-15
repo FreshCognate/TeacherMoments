baaba6ccb0a2b6c1a08199e7754f47c31d7a457f
darylhedley <darylhedley@hotmail.com>
2026-02-15 14:50:42 +0000

    Adds basic analytics to scenario results

--- frontend/modules/scenarios/containers/scenarioResultsContainer.jsx
+++ frontend/modules/scenarios/containers/scenarioResultsContainer.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import ScenarioResults from '../components/scenarioResults';
+import AnalyticsContainer from '~/modules/analytics/containers/analyticsContainer';
+import WithRouter from '~/core/app/components/withRouter';
+import WithCache from '~/core/cache/containers/withCache';
 
 class ScenarioResultsContainer extends Component {
   render() {
+
+    const { scenarioResponses } = this.props;
+
     return (
-      <ScenarioResults />
+      <AnalyticsContainer
+        responses={scenarioResponses.data}
+        isLoading={scenarioResponses.status === 'loading' || scenarioResponses.status === 'unresolved'}
+      />
     );
   }
 };
 
-export default ScenarioResultsContainer;
+export default WithRouter(WithCache(ScenarioResultsContainer, {
+  scenarioResponses: {
+    url: '/api/responses',
+    transform: ({ data }) => data.responses,
+    getQuery: ({ props }) => {
+      const { id } = props.router.params;
+      return {
+        scenarioId: id
+      };
+    },
+    getInitialData: () => ([])
+  }
+}));
