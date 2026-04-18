dafc998cfb2018d4d0ddf66843c1449b2bfebf93
darylhedley <darylhedley@hotmail.com>
2026-04-18 11:38:33 +0100

    Adds click to zoom feature

--- frontend/modules/blocks/components/imagesBlockPlayer.jsx
+++ frontend/modules/blocks/components/imagesBlockPlayer.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { AnimatePresence } from 'framer-motion';
 import getContent from '~/modules/ls/helpers/getContent';
 import map from 'lodash/map';
 import Image from '~/uikit/content/components/image';
+import ImageZoomOverlay from './imageZoomOverlay';
 import classnames from 'classnames';
 
 const ImagesBlockPlayer = ({
   items,
   imagesShape,
-  imagesBorderRadius
+  imagesBorderRadius,
+  canClickToZoom,
+  zoomedItem,
+  onImageClicked,
+  onZoomClosed
 }) => {
   let imageSize = 640;
   if (items.length > 1) {
     imageSize = 320;
   }
+
+  const zoomedAsset = zoomedItem ? getContent({ model: zoomedItem, field: 'asset' }) : null;
+
   return (
     <div className={classnames("grid gap-4", {
       "grid-cols-1": items.length === 1,
@@ -22,18 +31,43 @@ const ImagesBlockPlayer = ({
       {map(items, (item) => {
         const asset = getContent({ model: item, field: 'asset' });
         if (asset && asset._id) {
+          const image = (
+            <Image
+              asset={asset}
+              size={imageSize}
+              shape={imagesShape}
+              borderRadius={imagesBorderRadius}
+            />
+          );
+          if (canClickToZoom) {
+            return (
+              <button
+                key={item._id}
+                type="button"
+                className="cursor-zoom-in"
+                onClick={() => onImageClicked(item)}
+              >
+                {image}
+              </button>
+            );
+          }
           return (
             <div key={item._id}>
-              <Image
-                asset={asset}
-                size={imageSize}
-                shape={imagesShape}
-                borderRadius={imagesBorderRadius}
-              />
+              {image}
             </div>
           );
         }
       })}
+      <AnimatePresence>
+        {zoomedAsset && (
+          <ImageZoomOverlay
+            asset={zoomedAsset}
+            shape={imagesShape}
+            borderRadius={imagesBorderRadius}
+            onClose={onZoomClosed}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 };
