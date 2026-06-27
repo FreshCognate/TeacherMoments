07df4b6ea332c9df09754e47cf57fe5bd10fa0d5
darylhedley <darylhedley@hotmail.com>
2026-06-27 11:45:46 +0100

    Removes unused files

--- frontend/modules/triggers/containers/editTriggerContainer.jsx
+++ frontend/modules/triggers/containers/editTriggerContainer.jsx
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import EditTrigger from '../components/editTrigger';
-import getEditTriggerSchema from '../schemas/getEditTriggerSchema';
 import WithCache from '~/core/cache/containers/withCache';
 import getTrigger from '../helpers/getTrigger';
-import find from 'lodash/find';
 
 class EditTriggerContainer extends Component {
 
   getSchema = () => {
     const triggerName = this.props.modal.data.action;
     const trigger = getTrigger(triggerName);
-    return {
-      ...getEditTriggerSchema(),
-      ...trigger.getSchema(this.props.modal.data)
-    }
+    return trigger.getSchema(this.props.modal.data)
   }
 
   onFormUpdate = ({ update }) => {
