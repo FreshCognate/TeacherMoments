782c59c0124d3f3a2713223a23cb4e7e6ab5a049
darylhedley <darylhedley@hotmail.com>
2026-08-02 12:38:56 +0100

    Fixes prompt condition

--- frontend/modules/triggers/containers/editPromptConditionContainer.jsx
+++ frontend/modules/triggers/containers/editPromptConditionContainer.jsx
@@ -3,6 +3,7 @@ import EditPromptCondition from '../components/editPromptCondition';
 import find from 'lodash/find';
 import cloneDeep from 'lodash/cloneDeep';
 import includes from 'lodash/includes';
+import remove from 'lodash/remove';
 import WithCache from '~/core/cache/containers/withCache';
 
 class EditPromptConditionContainer extends Component {
@@ -11,7 +12,7 @@ class EditPromptConditionContainer extends Component {
     const { isMultiSelect, options } = this.props.prompt;
     const currentOption = find(options, { _id: selectedOptionId });
 
-    let usersSelectedOption = currentOption.value;
+    let usersSelectedOption = currentOption._id;
 
     let clonedSelectedOptions = cloneDeep(this.props.modal.data.selectedOptions);
 
