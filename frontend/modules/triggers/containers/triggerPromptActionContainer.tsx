18cf726c490df42a6e35df194a75454f985c8df0
darylhedley <darylhedley@hotmail.com>
2026-09-05 13:18:07 +0100

    Only adds stems if they need to be added

--- frontend/modules/triggers/containers/triggerPromptActionContainer.tsx
+++ frontend/modules/triggers/containers/triggerPromptActionContainer.tsx
@@ -6,6 +6,7 @@ import type { Scenario } from '~/modules/scenarios/scenarios.types';
 import handleRequestError from '~/core/app/helpers/handleRequestError';
 import axios from 'axios';
 import getCache from '~/core/cache/helpers/getCache';
+import getStemsBySlideRef from '~/modules/stems/helpers/getStemsBySlideRef';
 
 interface TriggerPromptActionContainerProps {
   slideRef: string;
@@ -24,19 +25,22 @@ class TriggerPromptActionContainer extends Component<TriggerPromptActionContaine
 
   onAddStemClicked = () => {
     createTrigger({ scenario: this.props.scenario.data._id, elementRef: this.props.slideRef, action: "BRANCH_TO_STEM_FROM_PROMPTS" });
-    axios.post('/api/stems', {
-      scenarioId: this.props.scenario.data._id,
-      slideRef: this.props.slideRef
-    }).then(() => {
-      const stems = getCache('stems');
-      if (stems && stems.fetch) {
-        stems.fetch()
-      }
-      const slides = getCache('slides');
-      if (slides && slides.fetch) {
-        slides.fetch()
-      }
-    }).catch(handleRequestError);
+    const stems = getStemsBySlideRef({ slideRef: this.props.slideRef });
+    if (stems.length < 2) {
+      axios.post('/api/stems', {
+        scenarioId: this.props.scenario.data._id,
+        slideRef: this.props.slideRef
+      }).then(() => {
+        const stems = getCache('stems');
+        if (stems && stems.fetch) {
+          stems.fetch()
+        }
+        const slides = getCache('slides');
+        if (slides && slides.fetch) {
+          slides.fetch()
+        }
+      }).catch(handleRequestError);
+    }
     this.props.onOpenTriggersClicked();
   }
 
